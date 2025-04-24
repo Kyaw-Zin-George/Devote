241877a034a211685b357b8aaa9f416c4a31e280
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-24 11:20:39 +0100

    adding data formatter

--- Devote/Utility/Constants.swift
+++ Devote/Utility/Constants.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import SwiftUI
+
+//Date Formatter
+
+ let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: - UI
+
+//MARK: - UX
