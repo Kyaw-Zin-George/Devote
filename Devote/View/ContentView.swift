8f826e7827ef370254e7af58ff919ef4d60e3032
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-24 11:22:04 +0100

    Adding save button, textfield to save a task in core data

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -9,43 +9,25 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    //MARK: - Property
+    @State var task: String = ""
+    private var isButtonDisabled: Bool { task.isEmpty }
+    //Fetching Data
     @Environment(\.managedObjectContext) private var viewContext
 
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
     private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }//List
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }//toolbar
-            Text("Select an item")
-        }
-    }
-
+    
+    //MARK: - function
     private func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
 
             do {
                 try viewContext.save()
@@ -55,6 +37,8 @@ struct ContentView: View {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+            task = ""
+            hideKeyboard()
         }
     }
 
@@ -65,21 +49,78 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
+    
+    //MARK: Body
+    var body: some View {
+        GeometryReader { geo in
+            NavigationView {
+                VStack {
+                    VStack(spacing: 6) {
+                        TextField("New Task",text: $task)
+                            .padding()
+                            .background(
+                                Color(UIColor.systemGray6)
+                            )
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                        
+                        Button(action: {
+                            addItem()
+                        } , label: {
+                            Spacer()
+                            Text("SAVE")
+                            Spacer()
+                        })
+                        .disabled(isButtonDisabled)
+                        .padding()
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .background( isButtonDisabled ? Color.gray :
+                            Color(UIColor.systemRed)
+                               
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        
+                    }
+                    .padding()
+                    List {
+                        ForEach(items) { item in
+                            
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                        .onDelete(perform: deleteItems)
+                    }//List
+                }//VStack
+                .navigationTitle("Daily Task")
+                .navigationBarTitleDisplayMode(.large)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
+                    }
+                    
+                }//ToolBar
+                
+                Text("Select an item")
+            }
+        }
+    }
+    
+    
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 #Preview {
     ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
