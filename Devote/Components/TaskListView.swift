fa977f571f6ae1c24293adee28f261b27d139bd4
Kyaw-Zin-George <ktz1695@gmail.com>
2025-04-26 23:34:40 +0100

    Refactoring list view

--- Devote/Components/TaskListView.swift
+++ Devote/Components/TaskListView.swift
@@ -8,29 +8,48 @@
 import SwiftUI
 
 struct TaskListView: View {
+    @State var task: String = ""
+    
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+    
+    //MARK: - function
+   
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
     var body: some View {
         List {
             ForEach(items) { item in
-                
-                VStack(alignment: .leading) {
-                    Text(item.task ?? "")
-                        .font(.headline)
-                        .fontWeight(.bold)
-                    
-                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                        .font(.footnote)
-                        .foregroundStyle(.gray)
-                }
+                ListRowItemView(item: item)
             }
             .onDelete(perform: deleteItems)
         }//List
+        .scrollContentBackground(.hidden)
+        .background(Color.clear)
         .listStyle(InsetGroupedListStyle())
         .shadow(color: Color(red: 0, green: 0, blue: 0), radius: 12)
         .padding()
-        .frame(maxWidth: 640)
+//     .frame(maxWidth: 640)
+        
     }
 }
 
 #Preview {
-    TaskListView()
+    TaskListView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        .background(Color.blue)
 }
